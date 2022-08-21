eb6c0f0184b69fd0d13eef57c337fef0f3b676e4
Benjamin Centner (PC) <bcentner@umich.edu>
2022-08-21 15:16:12 -0400

    cant think rn

--- algos.py
+++ algos.py
@@ -1,3 +1,4 @@
+# TODO: error checking on user input 
 def branchBound():
     domCount = input("how many dominos?\n")
     this_list = []
@@ -7,6 +8,25 @@ def branchBound():
         this_list.append([int(sample[0]), int(sample[1])])
 
     print(this_list)
-    strt = input("starting domino: ")
+    strt = int(input("starting domino: "))
     valid_starts = [x for x in this_list if x[0]== strt or x[1]==strt]
-    print(valid_starts)
+    
+    # no way to start
+    if len(valid_starts) < 1:
+        print("You have no starting options\n")
+        return 
+
+    longest = 0
+    count = 0
+    longest_li = []
+    cur = [valid_starts[0]]
+    for li_pair in valid_starts:
+        cur = [valid_starts[count]]
+        # FIXME: need loop here
+        if li_pair[0] == cur[-1]:
+            cur.append(li_pair[0])
+        elif li_pair[1] == cur[-1]:
+            cur.append(li_pair[-1])
+
+        count += 1
+        pass
