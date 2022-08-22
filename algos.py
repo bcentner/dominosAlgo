60cf50ffb4e1b48026b3c0d178be74475030014f
Benjamin Centner (PC) <bcentner@umich.edu>
2022-08-22 11:49:18 -0400

    some organizing

--- algos.py
+++ algos.py
@@ -1,20 +1,10 @@
 # TODO: error checking on user input 
-def branchBound():
-    domCount = input("how many dominos?\n")
-    this_list = []
-    for i in range(0, int(domCount)):
-        cur = input("numbers seperated by a space\n")
-        sample = cur.split()
-        this_list.append([int(sample[0]), int(sample[1])])
 
-    print(this_list)
-    strt = int(input("starting domino: "))
-    valid_starts = [x for x in this_list if x[0]== strt or x[1]==strt]
-    
-    # no way to start
-    if len(valid_starts) < 1:
-        print("You have no starting options\n")
-        return 
+
+def greedy():
+    pass
+
+def branchBound():
 
     longest = 0
     count = 0
@@ -22,7 +12,9 @@ def branchBound():
     cur = [valid_starts[0]]
     for li_pair in valid_starts:
         cur = [valid_starts[count]]
+
         # FIXME: need loop here
+        
         if li_pair[0] == cur[-1]:
             cur.append(li_pair[0])
         elif li_pair[1] == cur[-1]:
--- main.py
+++ main.py
@@ -1,11 +1,32 @@
 from algos import *
 
 def main():
+    print("---Welcome to Dominos---\n")
+    domCount = input("how many dominos?\n")
+    this_list = []
+    for i in range(0, int(domCount)):
+        cur = input("numbers seperated by a space\n")
+        sample = cur.split()
+        this_list.append([int(sample[0]), int(sample[1])])
+
+    print(this_list)
+    strt = int(input("starting domino: "))
+    valid_starts = [x for x in this_list if x[0]== strt or x[1]==strt]
+    
+    # no way to start
+    if len(valid_starts) < 1:
+        print("You have no starting options\n")
+        return 
+    pass
+
     txt = input("select algo type: ")
     print(txt)
     if int(txt)==1:
         print("branch and bound starting...")
         branchBound()
+    elif int(txt)==2:
+        print("starting greedy")
+        greedy()
 
 if __name__=='__main__':
     main()
